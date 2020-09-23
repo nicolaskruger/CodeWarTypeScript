f9d98d151772e7507e747ad1c5c0cfe5605e8547
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-23 16:47:37 -0300

    mySubmit

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('c'.repeat(11));
+Object.defineProperty(exports, "__esModule", { value: true });
+function dontGiveMeFive(start, end) {
+    var n = Array.from({ length: (1 + end - start) }, function (v, k) { return k + start; })
+        .filter(function (c) { return !c.toString().includes('5'); })
+        .length;
+    return n;
+}
+exports.dontGiveMeFive = dontGiveMeFive;
+console.log("ola");
+console.log(dontGiveMeFive(1, 9));
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log('c'.repeat(11));
+export function dontGiveMeFive(start:number, end:number) : number
+{
+    var n: number =
+    Array.from({length: (1+end - start)}, (v, k) => k + start)
+        .filter(c => !c.toString().includes('5'))
+        .length;
+    return n;
+}
+console.log("ola");
+console.log(dontGiveMeFive(1,9));
