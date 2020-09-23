f06472f0ff1111c1055ef5fcdb5240256c16f0e2
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-23 14:48:19 -0300

    001

--- index.ts
+++
@@ -1,9 +0,0 @@
-export function accum(s: string):string {
-    var i: number=0;
-    var str:string =
-    s.split("").map( s =>{
-        return s.toUpperCase()+s.toLocaleLowerCase().repeat(i++);
-    }).join("-");
-    return str;
-}
-console.log('a'.repeat(10));
