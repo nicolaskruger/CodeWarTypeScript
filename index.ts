852cf48f0ba96f4f515bbbb200defede077c8b13
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-23 11:05:38 -0300

    mysubmit

---
+++ index.ts
@@ -0,0 +1,11 @@
+export class G964 {
+
+    public static nbYear = (p0, percent, aug, p) => {
+        var i :number =0;
+        while(p0<p){
+            p0=p0+p0*percent/100+aug;
+            i++;
+        }
+        return i;
+    }
+}
