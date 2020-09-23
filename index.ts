c805118ade2734e0c7553228ded613484cb3476c
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-23 15:48:24 -0300

    mySubmit

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log('c'.repeat(11));
+export function findShort(s: string): number {
+    var str: string=
+    s.split(' ').sort( (a,b)=>{
+        if(a.length<b.length){
+            return -1;
+        }else if(a.length>b.length){
+            return 1;
+        }else return 0;
+    })[0];
+    return str.length;
+  }
