8d8388c9d3f5dc8d262e69f5c11b1f43a2a1fd76
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 20:01:38 -0300

    013ExecutionTimedOut

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,26 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class G964 {
+    static getAllDivisor(n) {
+        return Array.from({ length: n }, (_, i) => i + 1).filter(v => (n % v == 0));
+    }
+    static squareSum(n) {
+        return n.reduce((a, b) => a + b * b);
+    }
+    static isSquare(n) {
+        var sqr = Math.sqrt(n);
+        return ((sqr - Math.trunc(sqr)) == 0);
+    }
+    static range(start, end) {
+        return Array(end - start + 1).fill(1).map((_, idx) => start + idx);
+    }
+}
+G964.listSquared = (m, n) => {
+    return G964.range(m, n).map(i => {
+        let sum = G964.squareSum(G964.getAllDivisor(i));
+        return [i, sum];
+    }).filter(i => G964.isSquare(i[1]));
+};
+exports.G964 = G964;
+var val = G964.listSquared(1, 250);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
-console.log("teste");
+
+
+export class G964 {
+    public static getAllDivisor(n:number):Array<number>{
+        return Array.from({length: n}, (_, i) => i + 1).filter(v => (n%v==0));
+    }
+    public static squareSum(n:Array<number>):number{
+        return n.reduce((a,b)=>a+b*b);
+    }
+    public static isSquare(n:number):boolean{
+        var sqr= Math.sqrt(n);
+        return ((sqr-Math.trunc(sqr))==0)
+    }
+    public static range(start:number, end:number):Array<number> {
+        return Array(end - start + 1).fill(1).map((_, idx) => start + idx)
+      }
+    public static listSquared = (m, n) => {
+        return G964.range(m,n).map(i =>{
+            let sum = G964.squareSum(G964.getAllDivisor(i));
+            return [i,sum]
+        }).filter(i=> G964.isSquare(i[1]));
+    }
+}
+var val = G964.listSquared(1, 250);
