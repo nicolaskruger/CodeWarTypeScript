052dcbd3cd350a0a17af23df13f16e93bc3efe27
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 15:21:28 -0300

    firstTry

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log("teste");
+export function bouncingBall(h: number, bounce: number, window: number): number {
+    let cont:number =0;
+    while(h*bounce>window){
+        h*=bounce;
+        cont+=2;
+    }
+    return cont+1;
+  }
