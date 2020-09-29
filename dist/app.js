05b4d4fca73dabfdaf1ae7d822f8945013b3c9a2
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 20:44:57 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("teste");
+function solve(s) {
+    let uper = new RegExp("[A-Z]");
+    let lower = new RegExp("[a-z]");
+    let nUpper = s.split('').filter(s => uper.test(s)).length;
+    let nLower = s.split('').filter(s => lower.test(s)).length;
+    if (nLower > nUpper)
+        return s.toLowerCase();
+    if (nLower < nUpper)
+        return s.toUpperCase();
+    return s.toLowerCase();
+}
+exports.solve = solve;
+var str = solve("AAAAggRR");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
 console.log("teste");
+
+export function solve(s: string) {
+    let uper:RegExp = new RegExp("[A-Z]");
+    let lower:RegExp = new RegExp("[a-z]");
+    let nUpper= s.split('').filter(s => uper.test(s)).length;
+    let nLower= s.split('').filter(s => lower.test(s)).length;
+    if(nLower>nUpper) return s.toLowerCase();
+    if(nLower<nUpper) return s.toUpperCase();
+    return s.toLowerCase();
+}
+
+
+var str = solve("AAAAggRR");
