0901bb8342efb46f2a6315897eba42c7abaabd16
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 11:09:02 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,11 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// https://www.codewars.com/kata/52fba66badcd10859f00097e/train/typescript
+class Kata {
+    static disemvowel(str) {
+        return str.split('').filter(s => !"aeiouAEIOU".includes(s)).join("");
+    }
+}
+exports.Kata = Kata;
+var str = Kata.disemvowel("awwwqfqaegagdaAA");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 // https://www.codewars.com/kata/52fba66badcd10859f00097e/train/typescript
 export class Kata {
     static disemvowel(str: string) {
-      return str;
+      return str.split('').filter(s => !"aeiouAEIOU".includes(s)).join("");
     }
-  }
+}
+var str:string= Kata.disemvowel("awwwqfqaegagdaAA");
