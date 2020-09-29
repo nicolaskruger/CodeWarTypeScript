a5600b48f74fd2f0b557d3d78c0274378f40819e
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 15:55:48 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,13 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function bouncingBall(h, bounce, window) {
+    if (h <= window)
+        return -1;
+    let val = Math.floor(Math.log(window / h) / Math.log(bounce));
+    let hn = h * Math.pow(bounce, val);
+    let cont = 2 * val + (hn == window ? -2 : 0);
+    return cont + 1;
+}
+exports.bouncingBall = bouncingBall;
+bouncingBall(2, 0.5, 1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import { match } from "assert";
 export function bouncingBall(h: number, bounce: number, window: number): number {
-    let cont:number =0;
-    while(h*bounce>window){
-        h*=bounce;
-        cont+=2;
-    }
+    if(h<=window) return -1;  
+    let val:number =Math.floor(Math.log(window/h)/Math.log(bounce));
+    let hn = h*Math.pow(bounce,val);
+    let cont:number =2*val +(hn==window?-2:0);
+    if(cont<0||isNaN(cont)) return -1;
     return cont+1;
   }
+bouncingBall(2,0.5,1)
