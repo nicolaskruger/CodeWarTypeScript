4db3933b2247301f1b6d116663e54ff4a1bc6e75
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 11:09:41 -0300

    bestSubmit

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/52fba66badcd10859f00097e/train/typescript
 export class Kata {
     static disemvowel(str: string) {
-      return str.split('').filter(s => !"aeiouAEIOU".includes(s)).join("");
+        return str.replace(/[aeiou]/gi, "");
     }
 }
 var str:string= Kata.disemvowel("awwwqfqaegagdaAA");
