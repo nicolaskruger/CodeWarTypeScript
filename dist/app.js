a006919fc5b6b91b1325bab7929d14c475c458e8
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 13:44:20 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,6 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/typescript
+exports.digitize = (n) => (n.toString().split('').reverse().map(s => Number(s)));
+const n = exports.digitize(123);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
-console.log("teste");
+//https://www.codewars.com/kata/5583090cbe83f4fd8c000051/train/typescript
+export const digitize = (n: number): number[] => (
+    n.toString().split('').reverse().map(s => Number(s))
+);
+
+const n:number[] = digitize(123);
