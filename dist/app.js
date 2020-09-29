ffca071da07f5bd604e1df0340a035111e903d0b
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 11:47:57 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function even_or_odd(n) {
+    var dic = {
+        0: "Odd",
+        1: "Even"
+    };
+    return dic[n % 2];
+}
+exports.even_or_odd = even_or_odd;
+for (let index = 0; index < 3; index++) {
+    console.log(even_or_odd(index));
+}
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("teste");
+
+
+export function even_or_odd(n:number):String {
+    if(n<0) n=-n;
+    var dic = {
+          0:"Even",
+          1:"Odd"
+      }
+    return dic[n%2];
+}
+for (let index = 0; index < 3; index++) {
+    console.log(even_or_odd(index));    
+}
