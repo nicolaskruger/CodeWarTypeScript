90c7e1994f2f9b5976adc2edd9c9e5a57f3783e4
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 14:27:32 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,18 @@
-console.log("teste");
+"use strict";
+//https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/typescript
+Object.defineProperty(exports, "__esModule", { value: true });
+function longestConsec(strarr, k) {
+    let list = strarr.slice(0, k);
+    let bestString = list.join("");
+    strarr.forEach(el => {
+        list.shift();
+        list.push(el);
+        let newString = list.join("");
+        if (newString.length > bestString.length)
+            bestString = newString;
+    });
+    return bestString;
+}
+exports.longestConsec = longestConsec;
+console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("teste");
+//https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/typescript
+
+import { stringify } from "querystring";
+import { start } from "repl";
+
+export function longestConsec(strarr: string[], k: number): string {
+    if(k<=0||k>strarr.length) return '';
+    let list:string[] = strarr.slice(0,k);
+    let bestString:string= list.join("");
+    strarr.forEach(el => {
+        list.shift();
+        list.push(el);
+        let newString:string = list.join("");
+        if(newString.length>bestString.length) bestString=newString;
+    });
+    return bestString;
+}
+
+console.log(longestConsec(["zone","abigail","theta","form","libe","zas"], 2));
