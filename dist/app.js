9a74af72caf767887797e86565e7574bb132e3d2
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-29 17:13:53 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,16 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Kata {
+    static dnaStrand(dna) {
+        var dic = {
+            "A": "T",
+            "T": "A",
+            "G": "C",
+            "C": "G"
+        };
+        return dna.split("").map(s => dic[s]).join("");
+    }
+}
+exports.Kata = Kata;
+var str = Kata.dnaStrand("AAAA");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 export class Kata {
     static dnaStrand(dna: string) {
-        var dic
+        var dic:{[id:string]:string} = {
+            "A":"T",
+            "T":"A",
+            "G":"C",
+            "C":"G"
+        }
+        return dna.split("").map(s => dic[s]).join("");
     }
   }
+ 
+var str:string= Kata.dnaStrand("AAAA");
