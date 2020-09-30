b3cfc64389ff924f743141c1d4333ca9be50aa4e
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-09-30 15:25:34 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,19 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function tribonacci([a, b, c], n) {
+    // your code here
+    let arr = new Array(a, b, c);
+    let tribo = [...arr];
+    if (n < 3)
+        return arr.slice(0, n);
+    for (let index = 3; index < n; index++) {
+        let val = arr.reduce((s, j) => s + j);
+        tribo.push(val);
+        arr.push(val);
+        arr.shift();
+    }
+    return tribo;
+}
+exports.tribonacci = tribonacci;
+let l = tribonacci([1, 1, 1], 0);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("teste");
+export function tribonacci([a, b, c]: [number, number, number], n: number): number[] {
+    // your code here
+    let arr = new Array<number>(a,b,c);
+    let tribo = [...arr];
+    if(n<3) return arr.slice(0,n);
+    for (let index = 3; index < n; index++) {
+        let val = arr.reduce((s,j) => s+j)
+        tribo.push(val);
+        arr.push(val);
+        arr.shift();
+    }
+        
+    return tribo;
+}
+let l = tribonacci([1, 1, 1], 0);
