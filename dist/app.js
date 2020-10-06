552db37f5b64bc2ab3b6a4ca9f0f609d681babc0
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-06 17:16:25 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,16 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class G964 {
+    static partlist(arr) {
+        let matrix = [];
+        for (let i = 0; i < arr.length - 1; i++) {
+            let a = arr.slice(0, i + 1).join(" ");
+            let b = arr.slice(i + 1).join(" ");
+            matrix[i] = [a, b];
+        }
+        return matrix;
+    }
+}
+exports.G964 = G964;
+G964.partlist(["I", "wish", "I", "hadn't", "come"]);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("teste");
+import { strict } from "assert";
+import { stringify } from "querystring";
+
+export class G964 {
+  public static partlist(arr: string[]): string[][] {
+    let matrix:string[][] = [];
+    for (let i = 0; i <arr.length-1; i++) {
+      let a: string = arr.slice(0,i+1).join(" ");
+      let b: string = arr.slice(i+1).join(" ");
+      matrix[i]=[a,b];
+    }
+    return matrix;
+  }
+}
+G964.partlist(["I", "wish", "I", "hadn't", "come"]);
