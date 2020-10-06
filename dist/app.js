ac4f16081650dd6de5f7f4458978c5c0d564623c
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-06 14:24:13 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,19 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class G964 {
+}
+G964.productFib = (prod) => {
+    var a = 0;
+    var b = 1;
+    while ((a * b) <= prod) {
+        if ((a * b) == prod)
+            return [a, b, true];
+        var c = b + a;
+        a = b;
+        b = c;
+    }
+    return [a, b, false];
+};
+exports.G964 = G964;
+console.log(G964.productFib(4895));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("teste");
+export class G964 {
+
+    public static productFib = (prod) => {
+      var a = 0;
+      var b = 1;
+      while((a*b)<=prod){
+        if((a*b)==prod) return [a,b,true];
+        var c = b+a;
+        a = b;
+        b = c;
+      }
+      return [a,b,false];
+    }
+}
+
+console.log(G964.productFib(4895));
