f2db9c431fcfe6498d310e4d6f9fb018f4d54a57
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-06 15:24:20 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,10 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function number(busStops) {
+    let inb = busStops.map(x => x[0]).reduce((y, x) => y + x);
+    let ob = busStops.map(x => x[1]).reduce((y, x) => y + x);
+    return inb - ob;
+}
+exports.number = number;
+console.log(number([[10, 0], [3, 5], [5, 8]]));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,6 @@
-console.log("teste");
+export function number(busStops: [number, number][]): number {
+  let inb:number = busStops.map(x => x[0]).reduce((y,x)=>y+x);
+  let ob:number = busStops.map(x => x[1]).reduce((y,x)=>y+x);
+  return inb-ob;
+}
+console.log(number([[10,0],[3,5],[5,8]]));
