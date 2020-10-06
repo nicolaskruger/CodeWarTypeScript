6308830bbd059c03ffb10d0429af37bae293376e
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-06 15:25:02 -0300

    best

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,4 @@
-export function number(busStops: [number, number][]): number {
-  let inb:number = busStops.map(x => x[0]).reduce((y,x)=>y+x);
-  let ob:number = busStops.map(x => x[1]).reduce((y,x)=>y+x);
-  return inb-ob;
+export function number(busStops:number[][]):number {
+  return busStops.reduce((rem, [on, off]) => rem+(on-off), 0);
 }
 console.log(number([[10,0],[3,5],[5,8]]));
