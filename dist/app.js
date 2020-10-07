aa9dd69ebd51e43d4e3a0c85984bf43df684c8dc
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-07 09:04:39 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,26 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.encryptThis = (str) => {
+    // Your code goes here
+    if (str == "")
+        return "";
+    let st = str.split(" ");
+    st = st.map(s => {
+        if (s.length == 1) {
+            return s[0].charCodeAt(0).toString();
+        }
+        let val = s[0].charCodeAt(0);
+        s = s.slice(1);
+        s = val + s[s.length - 1] + s.slice(1, s.length - 1) + s[0];
+        return s;
+    });
+    return st.join(" ");
+};
+exports.encryptThis("");
+exports.encryptThis("A");
+exports.encryptThis("A wise old owl lived in an oak");
+exports.encryptThis("The more he saw the less he spoke");
+exports.encryptThis("The less he spoke the more he heard");
+exports.encryptThis("Why can we not all be like that wise old bird");
+exports.encryptThis("Thank you Piotr for all your help");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-console.log("teste");
+export const encryptThis = (str: string): string => {
+  // Your code goes here
+  if(str =="") return "";
+  let st = str.split(" ");
+  
+  st = st.map(s =>{
+    if(s.length==1){
+      return s[0].charCodeAt(0).toString();
+    }
+    let val= s[0].charCodeAt(0);
+    s= s.slice(1);
+    if(s.length==1) return val+ s[s.length-1];
+    s=val+ s[s.length-1]+s.slice(1,s.length-1)+s[0];
+    return s;
+  });
+  
+  return st.join(" ");
+}
+encryptThis("");
+encryptThis("A");
+encryptThis("A wise old owl lived in an oak"); 
+encryptThis("The more he saw the less he spoke");
+encryptThis("The less he spoke the more he heard");
+encryptThis("Why can we not all be like that wise old bird");
+encryptThis("Thank you Piotr for all your help");
