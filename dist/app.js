a1c1716440a509bccd151a744cf8d648cd7118b2
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-08 13:54:26 -0300

    mapResolution

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,36 @@
-console.log("teste");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class G964 {
+}
+G964.primeFactors = (n) => {
+    // your code
+    let dict = new Map();
+    let num = 2;
+    while (n != 1) {
+        if (n % num == 0) {
+            n /= num;
+            if (dict.has(num)) {
+                dict.set(num, dict.get(num) + 1);
+            }
+            else {
+                dict.set(num, 1);
+            }
+        }
+        else {
+            num++;
+        }
+    }
+    let str = "";
+    dict.forEach((v, k, m) => {
+        if (v == 1) {
+            str += `(${k})`;
+        }
+        else {
+            str += `(${k}**${v})`;
+        }
+    });
+    return str;
+};
+exports.G964 = G964;
+G964.primeFactors(86240);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log("teste");
+export class G964 {
+
+    public static primeFactors = (n) => {
+        // your code
+        let dict=new Map<number,number>();
+        let num =2;
+        while(n!=1){
+            if(n%num == 0){
+                n/=num;
+                if(dict.has(num)){
+                    dict.set(num,dict.get(num)+1);
+                }else{
+                    dict.set(num,1);
+                }
+            }else{
+                num++;
+            }
+        }
+        let str:string = ""
+        dict.forEach((v,k,m) =>{
+            if(v==1){
+                str +=`(${k})`;
+            }else{
+                str +=`(${k}**${v})`;
+            }
+        });
+        return str;
+    }
+}
+G964.primeFactors(86240);
