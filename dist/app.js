e83d8eb4f5c4dad3d77b53980477fa3a90033d7e
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-22 12:47:45 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class G964 {
-    static dec(n) {
-        return Math.sqrt(Array.from(Array(n + 1).keys()).map(s => s * s).filter(s => s <= n).pop());
+function findUniq(arr) {
+    // Do the magic
+    let dic = {};
+    arr.forEach(s => {
+        if (dic[s] == undefined) {
+            dic[s] = 1;
+        }
+        else {
+            dic[s]++;
+        }
+        return s;
+    });
+    for (const key in dic) {
+        if (dic[key] == 1) {
+            return key;
+        }
     }
+    ;
+    return 0;
 }
-G964.decompose = (n) => {
-    // your code
-    let vet = [];
-    n = n * n;
-    n--;
-    let m = G964.dec(n);
-    n++;
-    n -= m * m;
-    vet.push(m);
-    while (n > 0) {
-        m = G964.dec(n);
-        n -= m * m;
-        vet.push(m);
-    }
-    return vet.reverse();
-};
-exports.G964 = G964;
-console.log("teste");
-G964.decompose(50);
+exports.findUniq = findUniq;
+findUniq([1, 1, 1, 2, 1, 1]);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,22 @@
-export class G964 {
-
-    public static mathAll(str:string,rg:RegExp){
-        let vet  = [];
-        try {
-
-            while(true){
-               let st = str.match(rg)[0];
-               str = str.replace(st,'');
-               vet.push(st);
-            }
-        } catch (error) {
-            return vet;
+export function findUniq(arr: number[]): number {
+    // Do the magic
+    let dic: {[key:number]:number} = {}
+    arr.forEach(s =>{
+        if(dic[s]==undefined){
+            dic[s]=1;
         }
-    }
-    public static phone = (strng:string, num:string) => {
-        // your code
-        let numReg = /\d{1,2}-\d{3}-\d{3}-\d{4}/;
-        let nameReg = /<(.+)>/;
-        let endReg = /[\w.\-]+/;
-        
-        return strng.split('\n').map(s =>{
-            let numm = s.match(numReg);
-            let name = nameReg.exec(s);
-            let end = s.replace('+'+numm,'');
-            if(name!=null)
-            end = end.replace(name[0],'').trim();
-            end = G964.mathAll(end,endReg).join(" ").trim();
-            return [numm,name,end]
-        }).filter(s => s[0]==num).map(s => {
-            return `Phone => ${s[0]}, Name => ${s[1][1]}, Address => ${s[2]}`
-        })[0];
-    }
-}
+        else{
+            dic[s]++;
+        }
+        return s;
+    });
+
+    for (const key in dic) {
+        if (dic[key]==1) {
+            return Number.parseFloat(key);
+            
+        }
+    };
+    return 0;
+  }
+  findUniq([ 1, 1, 1, 2, 1, 1 ])
