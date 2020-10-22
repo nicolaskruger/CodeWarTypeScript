f6842be3189aa9cdbabcb04f569d059c18bbfa03
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-22 13:14:20 -0300

    bestSubmit

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,6 @@
-export function bTs(b:string){
-    let num =0;
-    let n = 7
-    b.split("").forEach(s => {
-        num |= Number.parseInt(s)<<n;
-        n--;
-    })
-    return String.fromCharCode(num);
-}
 
 export function binaryToString(binary: string) {
     
-    return binary.match(/.{8}/g).map(s => bTs(s)).join("");
-    
+    return binary.replace(/[01]{8}/g, s => String.fromCharCode(parseInt(s, 2)));
 }
 
-let l = binaryToString("0110000101100001");
