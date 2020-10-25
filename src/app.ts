e0662193537549eb1388db0107e4fadc61c63b85
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-25 20:30:03 -0300

    bestSubmit

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,6 @@
-const n = "NORTH";
-const s = "SOUTH";
-const w = "WEST";
-const e = "EAST";
-const oposit = {
-    "NORTH": s,
-    "SOUTH": n,
-    "WEST": e,
-    "EAST": w
-};
 export function dirReduc(arr: string[]): string[] {
-   let brr= arr.reduce((o,x)=>{
-        let last = o[o.length-1];
-        if(last==undefined){
-            o.push(x)
-            return o;
-        }
-        let op = oposit[last];
-        if(oposit[last]==x){
-            o.pop()
-            return o;
-        }
-        o.push(x);
-        return o;
-   },[]);
-   if(brr.length==arr.length){
-       return arr;
-   }
-   return dirReduc([...brr]);
-}
-let red = dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"]);
+    var pat = /(NORTHSOUTH|SOUTHNORTH|EASTWEST|WESTEAST)/;
+    var way = arr.join('');
+    while( pat.test(way) ) way = way.replace(pat,'');
+    return way.match(/(NORTH|SOUTH|EAST|WEST)/g) || [];
+  }
