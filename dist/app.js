26f03169a350051ad6c8451ef9ecae2a41df6ad8
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-25 20:09:33 -0300

    firstTry

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,44 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function areaOfPolygonInsideCircle(circleRadius, numberOfSides) {
-    return Number.parseFloat((Math.pow(circleRadius, 2) * Math.sin(2 * Math.PI / numberOfSides) * numberOfSides / 2).toFixed(3));
+const n = "NORTH";
+const s = "SOUTH";
+const w = "WEST";
+const e = "EAST";
+function giveMeTheList(num, word) {
+    let lis = `${word} `.repeat(Math.abs(num)).split(' ');
+    lis.pop();
+    return lis;
 }
-exports.areaOfPolygonInsideCircle = areaOfPolygonInsideCircle;
-let val = areaOfPolygonInsideCircle(3, 3);
+exports.giveMeTheList = giveMeTheList;
+function dirReduc(arr) {
+    let red = arr.reduce((o, x) => {
+        switch (x) {
+            case n:
+                o[0]++;
+                break;
+            case s:
+                o[0]--;
+                break;
+            case e:
+                o[1]++;
+                break;
+            case w:
+                o[1]--;
+                break;
+            default:
+                break;
+        }
+        return o;
+    }, [0, 0]);
+    let w0 = red[0] < 0 ? s : n;
+    let w1 = red[1] < 0 ? w : e;
+    let l0 = giveMeTheList(red[0], w0);
+    let l1 = giveMeTheList(red[1], w1);
+    if (arr.indexOf(w0) < arr.indexOf(w1))
+        return l0.concat(l1);
+    else
+        return l1.concat(l0);
+}
+exports.dirReduc = dirReduc;
+let red = dirReduc(['NORTH', 'EAST']);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,40 @@
-export function areaOfPolygonInsideCircle(circleRadius: number, numberOfSides: number): number {
-    return Number.parseFloat((Math.pow(circleRadius,2)*Math.sin(2*Math.PI/numberOfSides)*numberOfSides/2).toFixed(3));
-  }
-
- let val = areaOfPolygonInsideCircle(3, 3) ;
- 
+const n = "NORTH";
+const s = "SOUTH";
+const w = "WEST";
+const e = "EAST";
+export function giveMeTheList(num:number,word:string){
+    let lis = `${word} `.repeat(Math.abs(num)).split(' ');
+    lis.pop();
+    return lis;
+}
+export function dirReduc(arr: string[]): string[] {
+    let red = arr.reduce((o,x)=>{
+        switch (x) {
+            case n:
+                o[0]++;
+                break;
+            case s:
+                o[0]--;
+                break;
+            case e:
+                o[1]++;
+                    break;
+            case w:
+                o[1]--;
+                    break;
+        
+            default:
+                break;
+        }
+        return o;
+    },[0,0]);
+    let w0  = red[0]<0?s:n;
+    let w1  = red[1]<0?w:e;
+    let l0 = giveMeTheList(red[0], w0);
+    let l1 = giveMeTheList(red[1], w1);
+    if(arr.indexOf(w0)<arr.indexOf(w1))
+        return l0.concat(l1);
+    else
+        return l1.concat(l0);
+}
+let red = dirReduc([ 'NORTH', 'EAST' ]);
