20d1d9c61834451847d832371284cc1ff9c2abcc
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-10-26 17:16:05 -0300

    mySubmit

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,95 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class G964 {
-    static dec(n) {
-        return Math.sqrt(Array.from(Array(n + 1).keys()).map(s => s * s).filter(s => s <= n).pop());
+var grid = [];
+var current = Array(7).fill(5);
+const red = 1;
+const yellow = 2;
+const draw = 0;
+const decode = {
+    0: "Draw",
+    1: "Red",
+    2: "Yellow",
+    "Draw": 0,
+    "Red": 1,
+    "Yellow": 2,
+};
+const decodeLetter = (str) => (str.charCodeAt(0) - "A".charCodeAt(0));
+function createGrid() {
+    grid = [];
+    current = Array(7).fill(5);
+    for (let i = 0; i < 6; i++)
+        grid.push([...Array(7).fill(0)]);
+}
+function printGrid() {
+    grid.forEach(gg => {
+        console.log(gg);
+    });
+}
+function pushCard(color, pos) {
+    if (current[pos] != -1) {
+        let cur = current[pos];
+        grid[cur][pos] = color;
+        current[pos]--;
+        return [cur, pos];
+    }
+    else {
+        throw new Error();
     }
 }
-G964.decompose = (n) => {
-    // your code
-    let vet = [];
-    n = n * n;
-    n--;
-    let m = G964.dec(n);
-    n++;
-    n -= m * m;
-    vet.push(m);
-    while (n > 0) {
-        m = G964.dec(n);
-        n -= m * m;
-        vet.push(m);
+function line(y, x, dy, dx) {
+    try {
+        if (grid[y][x] == grid[y + dy][x + dx]) {
+            return 1 + line(y + dy, x + dx, dy, dx);
+        }
+        return 0;
     }
-    return vet.reverse();
-};
-exports.G964 = G964;
-console.log("teste");
-G964.decompose(50);
+    catch (error) {
+        return 0;
+    }
+}
+function win(y, x) {
+    let yy = line(y, x, 1, 0) + 1 + line(y, x, -1, 0);
+    let xx = line(y, x, 0, 1) + 1 + line(y, x, 0, -1);
+    let dr = line(y, x, -1, -1) + 1 + line(y, x, 1, 1);
+    let ur = line(y, x, 1, -1) + 1 + line(y, x, -1, 1);
+    let arr = [yy, xx, dr, ur];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= 4) {
+            let v = grid[y][x];
+            return v;
+        }
+    }
+    return draw;
+}
+function whoIsWinner(piecesPositionList) {
+    //return "Red", "Yellow" or "Draw"
+    createGrid();
+    for (const pp of piecesPositionList) {
+        let val = pp.split("_");
+        try {
+            let vet = pushCard(decode[val[1]], decodeLetter(val[0]));
+            let w = win(vet[0], vet[1]);
+            if (w != draw)
+                return decode[w];
+        }
+        catch (error) {
+        }
+    }
+    ;
+    return decode[draw];
+}
+let piecesPositionList = ["A_Yellow",
+    "B_Red",
+    "B_Yellow",
+    "C_Red",
+    "G_Yellow",
+    "C_Red",
+    "C_Yellow",
+    "D_Red",
+    "G_Yellow",
+    "D_Red",
+    "G_Yellow",
+    "D_Red",
+    "F_Yellow",
+    "E_Red",
+    "D_Yellow"];
+let valor = whoIsWinner(piecesPositionList);
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,91 @@
+var grid:Array<Array<number>> = [];
+var current:Array<number>= Array(7).fill(5);
+const red = 1;
+const yellow = 2;
+const draw = 0;
+const decode = {
+  0: "Draw",
+  1: "Red",
+  2: "Yellow",
+  "Draw":0,
+  "Red":1,
+  "Yellow":2,
+}
+const decodeLetter=(str:string)=> (str.charCodeAt(0) - "A".charCodeAt(0));
+function createGrid(){
+  grid = [];
+  current = Array(7).fill(5);
+  for(let i=0;i<6;i++)
+    grid.push([...Array(7).fill(0)])
+} 
+function printGrid(){
+  grid.forEach(gg=>{
+    console.log(gg);
+  });
+}
+function pushCard(color:number,pos:number):Array<number>{
+  if(current[pos]!=-1){
+    let cur = current[pos];
+    grid[cur][pos]=color;
+    current[pos]--;
+    return [cur,pos];
+  }else{
+    throw new Error();
+  }
+}
+function line(y:number,x:number,dy,dx){
+  try {
+    if(grid[y][x]==grid[y+dy][x+dx]){
+      return 1 + line(y+dy,x+dx,dy,dx);
+    }
+    return 0;
+  } catch (error) {
+    return 0;
+  }
+}
+function win(y:number,x:number){
+  let yy = line(y,x,1,0)+1+line(y,x,-1,0)
+  let xx = line(y,x,0,1)+1+line(y,x,0,-1)
+  let dr = line(y,x,-1,-1)+1+line(y,x,1,1)
+  let ur = line(y,x,1,-1)+1+line(y,x,-1,1)
+  let arr = [yy,xx,dr,ur];
+  for (let i = 0; i < arr.length; i++) {
+      if (arr[i] >= 4) {
+          let v= grid[y][x];
+          return v;      
+        }
+  }
+  return draw;
+}
+function whoIsWinner(piecesPositionList:Array<string>){
+  //return "Red", "Yellow" or "Draw"
+  createGrid();
+  for(const pp of piecesPositionList){
+    let val = pp.split("_")
+    try {
+      let vet = pushCard(decode[val[1]],decodeLetter(val[0]))
+      let w = win(vet[0],vet[1]);
+      if(w!=draw)
+        return decode[w];
+    } catch (error) {
+      
+    }
+  };
+  return decode[draw];
+}
+let piecesPositionList = ["A_Yellow",
+"B_Red",
+"B_Yellow",
+"C_Red",
+"G_Yellow",
+"C_Red",
+"C_Yellow",
+"D_Red",
+"G_Yellow",
+"D_Red",
+"G_Yellow",
+"D_Red",
+"F_Yellow",
+"E_Red",
+"D_Yellow"];
+let valor = whoIsWinner(piecesPositionList);
