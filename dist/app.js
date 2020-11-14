6ac0a52209587a893f1edb80e524dcb6dfc4b3db
nicolas-kruger <nicolas.nkruger@gmail.com>
2020-11-14 16:57:52 -0300

    001

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,36 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class G964 {
-    static dec(n) {
-        return Math.sqrt(Array.from(Array(n + 1).keys()).map(s => s * s).filter(s => s <= n).pop());
-    }
+function lcm_two_numbers(x, y) {
+    return (!x || !y) ? 0 : Math.abs((x * y) / gcd_two_numbers(x, y));
 }
-G964.decompose = (n) => {
-    // your code
-    let vet = [];
-    n = n * n;
-    n--;
-    let m = G964.dec(n);
-    n++;
-    n -= m * m;
-    vet.push(m);
-    while (n > 0) {
-        m = G964.dec(n);
-        n -= m * m;
-        vet.push(m);
+function gcd_two_numbers(x, y) {
+    x = Math.abs(x);
+    y = Math.abs(y);
+    while (y) {
+        var t = y;
+        y = x % y;
+        x = t;
     }
-    return vet.reverse();
+    return x;
+}
+function lcm(...list) {
+    return oper(lcm_two_numbers, 1, ...list);
+}
+function gcd(...list) {
+    return oper(gcd_two_numbers, list[0], ...list);
+}
+function oper(func, n, ...list) {
+    return list.reduce((t, s) => func(t, s), n);
+}
+exports.convertFrac = (lst) => {
+    let lista = lst.map(s => s[1]);
+    let val = lcm(...lista);
+    let li = lst.map(s => [s[0] * val / s[1], val]);
+    let num = li.reduce((s, t) => s + t[0], 0);
+    let gc = li.map(s => gcd_two_numbers(s[0], s[1]));
+    let gcd_ = gcd(...gc);
+    let l = li.map(s => `(${s[0] / gcd_},${s[1] / gcd_})`);
+    return l.join('');
 };
-exports.G964 = G964;
-console.log("teste");
-G964.decompose(50);
+let valor = exports.convertFrac([[1, 2], [4, 5], [3, 4], [6, 9], [7, 10]]);
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,35 @@
+function lcm_two_numbers(x:number, y:number) {
+    return (!x || !y) ? 0 : Math.abs((x * y) / gcd_two_numbers(x, y));
+ }
+ 
+ function gcd_two_numbers(x:number, y:number) {
+   x = Math.abs(x);
+   y = Math.abs(y);
+   while(y) {
+     var t = y;
+     y = x % y;
+     x = t;
+   }
+   return x;
+ }
+ function lcm(...list:number[]){
+    return oper(lcm_two_numbers,1,...list);
+ }
+ function gcd(...list:number[]){
+   return oper(gcd_two_numbers,list[0],...list);
+ }
+ function oper(func:(a:number,b:number)=>number,n,...list:number[]){
+    return list.reduce((t,s)=>func(t,s),n);
+ }
+export const convertFrac = (lst: [number, number][]): string => {
+    let lista = lst.map(s=>s[1]);
+    let val = lcm(...lista);
+    let li = lst.map(s=>[s[0]*val/s[1],val]);
+    let num = li.reduce((s,t)=>s+t[0],0)
+    let gc = li.map(s=>gcd_two_numbers(s[0],s[1]));
+    let gcd_ = gcd(...gc);
+    let l = li.map(s=>`(${s[0]/gcd_},${s[1]/gcd_})`)
+    return l.join('');
+}
+ 
+let valor = convertFrac([[1, 2], [4, 5], [3, 4], [6, 9], [7, 10]]);
